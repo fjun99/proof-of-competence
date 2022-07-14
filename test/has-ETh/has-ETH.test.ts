cc9d6099e47593a35bfb1ac301e0c28501359b29
fangjun <fjun99@gmail.com>
2022-07-14 14:55:12 +0800

    test edit

--- test/has-ETh/has-ETH.test.ts
+++ test/has-ETh/has-ETH.test.ts
@@ -1,11 +1,13 @@
 import { expect } from 'chai';
-import { Contract } from 'ethers';
+// import { Contract } from 'ethers';
 import { MockProvider } from 'ethereum-waffle';
 import { Task, Quest } from "../../src/types"
-import { resolve, join } from 'path'
+import { join } from 'path'
 import  fs  from 'fs'
 
-
+/*************************************
+//  verifyScore in utils/verify.ts
+*************************************/
 export async function verifyScore(task: Task, address?: string | null) {
   if (!address) return false
 
@@ -26,6 +28,7 @@ export async function verifyScore(task: Task, address?: string | null) {
   return results.some(i => i)
 }
 
+
 const filename = 'testquest.json'
 const fullPath = join(__dirname, filename)
 
@@ -34,39 +37,36 @@ if (!content) {
     console.log('File has no content..', filename)
 }
 
-let quest
-
-if (content) {
-  quest = JSON.parse(content) as Quest 
-  quest.id = filename.replace('.json', '')
-}
+let quest = JSON.parse(content) as Quest 
+quest.id = filename.replace('.json', '')
 
 describe('verifier - has-ETH', () => {
-  const [wallet, walletTo] = new MockProvider().getWallets();
-  let token: Contract;
+  // const [wallet, walletTo] = new MockProvider().getWallets();
+  // let token: Contract;
 
 //   beforeEach(async () => {
 //     token = await deployContract(wallet, BasicToken, [1000]);
 //   });
 
   it('should return true', async () => {
-
     const address = '0xF0DA8606a337fe16bd1F8845CeE85079AC702300'
     expect(await verifyScore(quest.tasks[0],address)).to.be.true;
     expect(await verifyScore(quest.tasks[1],address)).to.be.true;
+    expect(await verifyScore(quest.tasks[2],address)).to.be.true;
+    expect(await verifyScore(quest.tasks[3],address)).to.be.true;
 
   });
 
   it('should return false', async () => {
-
     const address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
     expect(await verifyScore(quest.tasks[0],address)).to.be.false;
     expect(await verifyScore(quest.tasks[1],address)).to.be.false;
+    expect(await verifyScore(quest.tasks[2],address)).to.be.false;
+    expect(await verifyScore(quest.tasks[3],address)).to.be.false;
 
   });
 
 });
-
 
 //Error: Timeout of 2000ms exceeded. For async tests and hooks, ensure "done()" is called; 
 // if returning a Promise, ensure it resolves. 
