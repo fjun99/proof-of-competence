068237e901191d0085523f0246cac10a2c1f3182
fangjun <fjun99@gmail.com>
2022-07-29 11:39:48 +0800

    we quest

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
-export const TITLE = 'Web3 Quest'
+export const TITLE = 'Web3 之旅'
 export const DESCRIPTION = 'Proof of Competence (PoC) is an open, modular framework to create on-chain quests and on-boarding journeys into Web3.'
-export const SITE_URL = `https://www.poc.quest/`
+export const SITE_URL = `https://www.huo3.xyz/`
 export const IMAGE_OG = `${SITE_URL}images/social.png`
 export const SOCIAL_HANDLE = '@proofofcompetence'
 
